fb2e3edf78cf2906700ffb32b4e41023a111b1b9
Maximus32 <rgaiser@gmail.com>
2020-03-17 12:14:35 +0100

    Add default crt0.o

--- gcc/config/mips/ps2sdk.h
+++ gcc/config/mips/ps2sdk.h
@@ -7,3 +7,9 @@
         -lps2sdkc \
         -lkernel \
     --end-group"
+
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC "crt0.o%s crti.o%s crtbegin.o%s"
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC "crtend.o%s crtn.o%s"
